5e3515c948caea7ec05a42f72b6f18b65ca3fffa
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-11-25 23:27:45 +0700

    Create skeleton of project.

---
+++ src/main/java/com/RPL/SiapSidang/SiapSidangApplication.java
@@ -0,0 +1,13 @@
+package com.RPL.SiapSidang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiapSidangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiapSidangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SiapSidang/SiapSidangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SiapSidang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiapSidangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
