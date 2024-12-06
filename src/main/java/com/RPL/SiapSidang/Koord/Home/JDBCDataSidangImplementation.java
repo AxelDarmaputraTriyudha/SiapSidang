09a3e5bb07c042107b3896922e4d83e807710f81
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-06 22:15:37 +0700

    Implementasi filter selesai

--- src/main/java/com/RPL/SiapSidang/Koord/Home/JDBCDataSidangImplementation.java
+++ src/main/java/com/RPL/SiapSidang/Koord/Home/JDBCDataSidangImplementation.java
@@ -2,6 +2,8 @@ package com.RPL.SiapSidang.Koord.Home;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +15,41 @@ public class JDBCDataSidangImplementation {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public List<DataSidang> findAll(){
+    public List<DataSidang> findAll(String filter,
+                                    LocalDate tgl_awal,
+                                    LocalDate tgl_akhir,
+                                    String semester,
+                                    int tahun){
+
         String sql = "SELECT * FROM data_sidang_view WHERE peran = 'Koordinator'";
-        return jdbcTemplate.query(sql, this::mapRowToDataSidang);
+        
+        // kumpulan filter yang digunakan
+        List<Object> filterList = new ArrayList<>();
+
+        // cari filter apa aja yang dimasukkin
+        // kalo filternya ada, tambahin ke query SQL nya
+        if(filter.length() > 0){
+            sql += " AND nama ILIKE ?";
+            filterList.add("%" + filter + "%");
+        }
+        if(tgl_awal != null){
+            sql += " AND tanggal >= ?";
+            filterList.add(tgl_awal);
+        }
+        if(tgl_akhir != null){
+            sql += " AND tanggal <= ?";
+            filterList.add(tgl_akhir);
+        }
+        if(semester.length() > 0){
+            sql += " AND semester_akd = ?";
+            filterList.add(semester);
+        }
+        if(tahun > 0){
+            sql += " AND tahun_akd = ?";
+            filterList.add(tahun);
+        }
+
+        return jdbcTemplate.query(sql, this::mapRowToDataSidang, filterList.toArray());
     }
 
     private DataSidang mapRowToDataSidang(ResultSet resultSet, int rowNum) throws SQLException{
@@ -33,10 +67,5 @@ public class JDBCDataSidangImplementation {
             resultSet.getString("nama_dosen"),
             resultSet.getString("peran")
         );
-    }
-    
-    public List<DataSidang> findWithFilter(String filter){
-        String sql = "SELECT * FROM data_sidang_view WHERE nama ILIKE ? AND peran = 'Koordinator'";
-        return jdbcTemplate.query(sql, this::mapRowToDataSidang, "%"+filter+"%");
     }
 }
