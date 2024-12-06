1ca0484927f4b25493cae3372651107e29d28d28
AxelDarmaputraTriyudha <6182201053@student.unpar.ac.id>
2024-12-07 00:26:51 +0700

    Implementasi halaman homepage untuk dosen

---
+++ src/main/java/com/RPL/SiapSidang/Dosen/Home/DosenHomepageController.java
@@ -0,0 +1,48 @@
+package com.RPL.SiapSidang.Dosen.Home;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.RPL.SiapSidang.Koord.Home.DataSidang;
+
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/dosen")
+public class DosenHomepageController {
+    @Autowired
+    JDBCDosenRepositoryImplementation dosenRepository;
+
+    @GetMapping("/home")
+    public String index(Model model,
+            @RequestParam(required = false, defaultValue = "") String filter,
+            @RequestParam(required = false, defaultValue = "") LocalDate tgl_awal,
+            @RequestParam(required = false, defaultValue = "") LocalDate tgl_akhir,
+            @RequestParam(required = false, defaultValue = "") String semester,
+            @RequestParam(required = false, defaultValue = "0") int tahun,
+            HttpSession session){
+
+        // test with dummy session
+        session.setAttribute("nik", "20010002");
+        session.setAttribute("nama", "Keenan");
+
+        List<DataSidang> dataSidangList = dosenRepository.findAll(filter, tgl_awal, tgl_akhir, semester, tahun, (String) session.getAttribute("nik"));
+
+
+        model.addAttribute("tgl_awal", tgl_awal);
+        model.addAttribute("tgl_akhir", tgl_akhir);
+        model.addAttribute("filter", filter);
+        model.addAttribute("sidang", dataSidangList);
+        model.addAttribute("semester", semester);
+        model.addAttribute("tahun", tahun);
+        return "/dosen/index";
+    }
+}
