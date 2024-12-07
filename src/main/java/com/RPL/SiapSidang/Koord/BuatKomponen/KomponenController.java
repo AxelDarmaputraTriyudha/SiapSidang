3b96f9fc004ca86743ff587f8e509e46b9c4107c
TiffanyTasya <6182201017@student.unpar.ac.id>
2024-12-07 09:34:29 +0700

    Controller untuk Komponen

---
+++ src/main/java/com/RPL/SiapSidang/Koord/BuatKomponen/KomponenController.java
@@ -0,0 +1,14 @@
+package com.RPL.SiapSidang.Koord.BuatKomponen;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/koord")
+public class KomponenController {
+    @GetMapping("/buatKomponen")
+    public String index(){
+        return "koord/BuatKomponen/index1";
+    }    
+}
