cc9e7b528ff8d1b178ab2250beaf8a3caa8b36bb
Naia Azzahra Sugandi <naiaazzahra.2701@gmail.com>
2024-12-12 12:04:04 +0700

    Menambahkan controller CatatanSidang

--- src/main/java/com/RPL/SiapSidang/Pembimbing/CatatanSidang/CatatanSidangController.java
+++ src/main/java/com/RPL/SiapSidang/Pembimbing/CatatanSidang/CatatanSidangController.java
@@ -1,15 +1,24 @@
 package com.RPL.SiapSidang.Pembimbing.CatatanSidang;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping("/pembimbing/catatan")
 public class CatatanSidangController {
 
     @Autowired
     private JDBCCatatan jdbcCatatan;
+
+    // Method untuk menampilkan halaman catatan
+    @GetMapping
+    public String showCatatanPage(Model model) {
+        // Anda dapat menambahkan atribut model jika diperlukan
+        return "/pembimbing/CatatanSidang/catatan"; // Mengembalikan nama view catatan.html
+    }
 
     @GetMapping("/data")
     public Map<String, Object> getData(@RequestParam int idTugasAkhir) {
